98daea996b178f9bd6037639d700289b51e221d6
Janet Yu <janet@Janets-MacBook-Air.local>
2025-09-16 23:16:53 -0500

    fix 401 response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
 
   before_action :authenticate_user!
+
+  def authenticate_user!(opts = {})
+    if user_signed_in?
+      super
+    else
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  end
 end
