6f66f43dba1112d264361c57b60c76f0c7af1ae0
Janet Yu <janet@Janets-MacBook-Air.local>
2025-09-19 22:52:48 -0500

    try 5 times rate limit

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,6 @@
 class Rack::Attack
   # 1. Throttle logins: max 5 requests per 20 seconds
-  throttle('logins/ip', limit: 4, period: 20.seconds) do |req|
+  throttle('logins/ip', limit: 5, period: 20.seconds) do |req|
     if req.path == '/users/sign_in' && req.post?
       # req.ip
       body = req.body.read
