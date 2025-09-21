3e621d4bde4cac46536d5ec726f243246bdac086
Janet Yu <janet@Janets-MacBook-Air.local>
2025-09-21 10:11:22 -0500

    Rubocop fixes

--- lib/middleware/extract_email.rb
+++ lib/middleware/extract_email.rb
@@ -6,12 +6,12 @@ module Middleware
 
     def call(env)
       req = Rack::Request.new(env)
-      if req.path == '/users/sign_in' && req.post?
+      if req.path == "/users/sign_in" && req.post?
         body = req.body.read
         req.body.rewind
         data = JSON.parse(body) rescue {}
-        email = data['email'] || data.dig('user', 'email')
-        env['HTTP_X_EMAIL'] = email if email
+        email = data["email"] || data.dig("user", "email")
+        env["HTTP_X_EMAIL"] = email if email
       end
       @app.call(env)
     end
